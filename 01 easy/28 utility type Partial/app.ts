d64c31945960be48be5acca0d7b5e87cdda7622b
Peter <hoffmannpeter82@gmail.com>
2022-06-19 09:29:11 +0200

    Extend Example Partial

--- 01 easy/28 utility type Partial/app.ts
+++ 01 easy/28 utility type Partial/app.ts
@@ -1,5 +1,6 @@
 /**
  * Partial is a specific "Utility Type". It can be used to define a variable that must only contain of parts of a given type.
+ * At the end it takes a type T and returns another type with the same shape as T but with all fields set to optional.
  */
 
 interface ICourse {
@@ -18,3 +19,10 @@ interface ICourse {
  * By using Partial it would work. Or properties are considered as optional. And you only have access to the properties of [ICourse].
  */
 const myCourse: Partial<ICourse> = {};
+
+/**
+ * This is how Partial is declared
+ */
+type MyPartial<T> = {
+  [P in keyof T]?: T[P];
+};
