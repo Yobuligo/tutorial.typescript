193d8c2a1c4cb27731134aacc4b177c14621cd95
Peter <hoffmannpeter82@gmail.com>
2022-06-19 09:45:54 +0200

    Extend Example Readonly

--- 01 easy/29 utility type Readonly/app.ts
+++ 01 easy/29 utility type Readonly/app.ts
@@ -1,5 +1,6 @@
 /**
  * Readonly is a specific "Utility Type". It can be used to define that a variable or even properties of an object are readonly.
+ * At the end it takes a type T and returns another type with the same shape as T but with all fields set to readonly.
  */
 
 const myList: Readonly<string[]> = ["Stacey", "Bertha"];
@@ -9,11 +10,14 @@ const myList: Readonly<string[]> = ["Stacey", "Bertha"];
  */
 // myList.push("123")
 
-
-class ReadOnlyObject{
-    name: string = ""
-    title: string = ""
+class ReadOnlyObject {
+  name: string = "";
+  title: string = "";
 }
 
-const readOnlyObject: Readonly<ReadOnlyObject> = new ReadOnlyObject()
+const readOnlyObject: Readonly<ReadOnlyObject> = new ReadOnlyObject();
 // readOnlyObject.name = "123"
+
+type MyReadOnly<T> = {
+  readonly [K in keyof T]: T[K];
+};
