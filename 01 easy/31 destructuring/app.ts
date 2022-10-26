439d07a7aa41fdd53f7b603c5dc05d1e049abba2
vtewes <tewes.verena@gmail.com>
2022-10-26 12:41:09 +0200

    Add aliasing for destructured objects

--- 01 easy/31 destructuring/app.ts
+++ 01 easy/31 destructuring/app.ts
@@ -6,5 +6,12 @@ const myArray = [1, 2, 3];
 const [one, , three] = myArray;
 println(one, three);
 
-const myLeaf = { isLeaf: true, name: "My Name" };
+const myLeaf = { isLeaf: true, name: "My leaf name" };
 const { isLeaf } = myLeaf;
+
+//Destructured properties of objects can be aliased, e.g. when two properties have the same name
+const myOakTree = { isLeaf: true, name: "Oak" };
+const myPineTree = { isLeaf: false, name: "Pine" };
+//aliases for names
+const { name: oakName } = myOakTree;
+const { name: pineName } = myPineTree;
