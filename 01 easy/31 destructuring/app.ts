8d13867fefa1e7821a8eb27cae25dfad72d539ae
Yobuligo <hoffmannpeter82@gmail.com>
2022-10-26 15:05:43 +0200

    Pretty print comments

--- 01 easy/31 destructuring/app.ts
+++ 01 easy/31 destructuring/app.ts
@@ -9,9 +9,9 @@ println(one, three);
 const myLeaf = { isLeaf: true, name: "My leaf name" };
 const { isLeaf } = myLeaf;
 
-//Destructured properties of objects can be aliased, e.g. when two properties have the same name
+// Destructured properties of objects can be aliased, e.g. when two properties have the same name
 const myOakTree = { isLeaf: true, name: "Oak" };
 const myPineTree = { isLeaf: false, name: "Pine" };
-//aliases for names
+// aliases for names
 const { name: oakName } = myOakTree;
 const { name: pineName } = myPineTree;
