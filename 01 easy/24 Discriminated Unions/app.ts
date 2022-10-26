f15366a8e2d683a60e0b8fb0748f7f34b5e90b86
vtewes <tewes.verena@gmail.com>
2022-10-26 12:32:04 +0200

    Improve comments

--- 01 easy/24 Discriminated Unions/app.ts
+++ 01 easy/24 Discriminated Unions/app.ts
@@ -1,6 +1,6 @@
 // An approach some kind of workaround for the reason that interface are no real types in javascript.
 // Instead any interface gets a property e.g. type or whatever.
-// Later we use a intersection type to combine multiple interfaces.
+// Later we use an intersection type to combine multiple interfaces.
 // To differ the interface (either IBird or IHorse) the property type is read, which is available for both interfaces.
 // So it is possible to access in a type safe way either the property flyingSpeed for IBird or runningSpeed for IHorse.
 // It is even interesting that "bird" and "horse" are suggested in the switch statement by the compiler.
