e5b4d798bc7fedd5d0c0904e81ae2d8de8b1604c
Yobuligo <hoffmannpeter82@gmail.com>
2022-12-09 12:09:29 +0100

    Extend example for spread and rest operator

--- 01 easy/20 spread operator/app.ts
+++ 01 easy/20 spread operator/app.ts
@@ -1,5 +1,7 @@
 import { println } from "../../GlobalFunctions";
 
+// Also see examples on https://plusreturn.com/blog/6-awesome-tricks-with-the-spread-and-rest-operators-in-typescript-and-javascript-objects/
+
 // Copy all values from one array in another
 let values = ["one", "two", "three"];
 let values2 = [...values];
@@ -49,3 +51,19 @@ const mySecondPerson = {
 
 // So the spread operator can be used to clone / copy objects
 const myThirdPerson = { ...myFirstPerson };
+
+// It is even possible to copy several objects into one.
+// Here a new object 'myFourthPerson' is initialized with the properties of 'myFirstPerson' and 'cat'
+// In case the objects would have the same properties, the last would win. This means if cat also had a prop 'firstname', its value would win.
+const cat = {
+  name: "Kitty",
+  age: 3,
+};
+const myFourthPerson = { ...myFirstPerson, ...cat };
+
+// Resetting properties while copying. Here a copy of 'myFirstPerson' is created but the firstname is directly reset to 'Changed Firstname'
+const myFifthPerson = { ...myFirstPerson, firstname: "Changed Firstname" };
+
+// Adding new properties. Here a copy of 'myFirstPerson' is created which gets a new property 'age'
+const mySixthPerson = { ...myFirstPerson, age: 28 };
+console.log(mySixthPerson.age);
