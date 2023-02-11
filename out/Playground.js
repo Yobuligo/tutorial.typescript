a2dcf04b1a929beb983eaa54d93d536af23a89a7
Yobuligo <hoffmannpeter82@gmail.com>
2023-02-11 23:58:48 +0100

    Add example for service provider

--- out/Playground.js
+++ out/Playground.js
@@ -1,1 +1,36 @@
+var MyService;
+(function (MyService) {
+    MyService[MyService["DAO"] = 0] = "DAO";
+    MyService[MyService["FACTORY"] = 1] = "FACTORY";
+})(MyService || (MyService = {}));
+class AnyFactory {
+    create() {
+        throw new Error("Method not implemented.");
+    }
+}
+class Logger {
+    log(message) {
+        throw new Error("Method not implemented.");
+    }
+}
+class ServiceProvider {
+    constructor() {
+        this.services = [];
+    }
+    add(type, service) {
+        this.services.push({ type: type, service: service });
+    }
+    register(type, instanceType) {
+        // this.services.push({ type: type, service: service });
+    }
+    fetch(type) {
+        return this.services.find((serviceRegistration) => {
+            return serviceRegistration.type === type;
+        });
+    }
+}
+const serviceProvider = new ServiceProvider();
+const anyFactory = serviceProvider.fetch("IAnyFactory");
+serviceProvider.register("IAnyFactory", AnyFactory);
+const logger = serviceProvider.fetch("ILogger");
 //# sourceMappingURL=Playground.js.map
