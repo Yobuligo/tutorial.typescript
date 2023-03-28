86583b1d39b6aacee650e9d6133be3b6528c2995
Yobuligo <hoffmannpeter82@gmail.com>
2023-03-28 21:43:04 +0200

    Delete out

--- out/01 easy/33 tuples/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Tuples are like structures or records. They have a fix set of fields like a row in a table.
-// Each field has a name.
-var myTuple = [1, "Stacey", "Starfish"];
-// Tuples can be organized in lists
-var myTuples = [];
-myTuples.push(myTuple);
-myTuples.push([2, "Robin", "Rabbit"]);
-//# sourceMappingURL=app.js.map
