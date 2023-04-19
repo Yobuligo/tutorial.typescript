b4f9ed95b418de884bcbdb3d8842cc7ee27b25ab
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-19 12:02:12 +0200

    Add lesson for getting only keys of enum

--- Playground.ts
+++ Playground.ts
@@ -1,1 +1,13 @@
-console.log(`Test`)
+namespace EnumKeys {
+    enum Gender {
+      Unknown,
+      Male,
+      Female,
+    }
+  
+    const elements = Object.keys(Gender).filter(
+      (element) => !(parseInt(element) >= 0)
+    );
+    console.log(elements);
+  }
+  
