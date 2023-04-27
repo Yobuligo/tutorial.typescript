029dd860f1cf1ee1f9d83b7a8a84ff484e38c710
Yobuligo <hoffmannpeter82@gmail.com>
2023-04-27 21:26:50 +0200

    Add lesson for creating uuids

--- Playground.ts
+++ Playground.ts
@@ -1,13 +1,2 @@
-namespace EnumKeys {
-    enum Gender {
-      Unknown,
-      Male,
-      Female,
-    }
-  
-    const elements = Object.keys(Gender).filter(
-      (element) => !(parseInt(element) >= 0)
-    );
-    console.log(elements);
-  }
-  
+// const uuid = crypto.randomUUID();
+// console.log(uuid);
