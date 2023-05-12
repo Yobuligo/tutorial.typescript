107050bcbba11c682cf5447c618096cf2c892f74
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-12 15:09:05 +0200

    Group nullish coalescing assignment operator to logical assignment operators

--- 01 easy/44 Logical nullish coalescing assignment operator/app.ts
+++ 01 easy/44 Logical nullish coalescing assignment operator/app.ts
@@ -1,5 +1,8 @@
 /**
  * The nullish coalescing operator can be combined with the equal sign to directly reassign the value itself if it is not null or otherwise the fallback value at the right side.
+ * 
+ * The nullish coalescing operator is pretty similar to the Logical OR Assignment operator ||=, the difference is that the nullish coalescing operator will check the value on the left side only against null or undefined.
+ * On the other hand the Logical OR Assignment operator checks against all falsy values, which includes 0 or an empty string "".
  */
 
 namespace NullishCoalescingAssignment {
