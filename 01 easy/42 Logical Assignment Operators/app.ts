552e9f5fe0d85d9551b9b7e4f75c612e10a9f9c5
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-12 15:26:00 +0200

    Add short overview with examples for AND, OR and Nullish Coalescing operator

--- 01 easy/42 Logical Assignment Operators/app.ts
+++ 01 easy/42 Logical Assignment Operators/app.ts
@@ -4,6 +4,20 @@
  * - nullish coalescing assignment
  * - and assignment
  * - or assignment
- * 
- * For more details see the concretes examples in the lessons below
+ *
+ * For more details see the concretes examples in the lessons below (within the explorer). Below there are only small examples
  */
+
+namespace LogicalAssignmentOperator {
+  // Trim values, if value is truthy
+  let firstname = "Stacey";
+  firstname &&= firstname.trim();
+
+  // Initialize value, if value is falsy
+  let lastname = "";
+  lastname ||= "Starfish";
+
+  // Provide alternative value, if value is null or undefined
+  let person: { firstname: string } | undefined = undefined;
+  person ??= { firstname: "Alex" };
+}
