f17135f1e98f4a14bdb52bc0546a39a10f9d5497
Yobuligo <hoffmannpeter82@gmail.com>
2023-05-12 15:22:24 +0200

    Restructure Logical Assignment Operators

---
+++ 01 easy/42 Logical Assignment Operators/app.ts
@@ -0,0 +1,9 @@
+/**
+ * Logical Assignment Operators are operators which check a condition and depending on the result a value is assigned.
+ * Here we have:
+ * - nullish coalescing assignment
+ * - and assignment
+ * - or assignment
+ * 
+ * For more details see the concretes examples in the lessons below
+ */
