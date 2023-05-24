c70f0180975f05da542e36e4414d1afc2568d464
Peter <hoffmannpeter82@gmail.com>
2023-05-24 17:54:38 +0200

    Add lesson for named and default export

---
+++ 01 easy/41 named export vs default export/app.ts
@@ -0,0 +1,11 @@
+/**
+ * There are two types if exports
+ *      1. named export
+ *      2. default export
+ * 
+ * The default export means to export only one main program construct like a function, constant, class, interface, etc. from a file.
+ * It can be achieved by writing "export default <Name>" at the end or probably also at the beginning of a file.
+ * 
+ * The named export means to export several program (or even only one) constructs like functions, constants, classes, interfaces, combinations, etc.
+ * It can be achieved by writing the keyword "export" directly in front of the program construct.
+ */
