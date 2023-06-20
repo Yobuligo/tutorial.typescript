a1f676cdf1da2c46a19f77578b6857b05b606cef
Peter <hoffmannpeter82@gmail.com>
2023-06-20 21:26:19 +0200

    Add lesson for creating interfaces with indexed type

---
+++ 01 easy/26-1 index properties with fix interface properties.ts/app.ts
@@ -0,0 +1,23 @@
+/**
+ * The previous example shows that it is possible to define an interface with a variable amount of properties.
+ * But in some cases I want to have a property with properties, which should have all the same type, but I want to know these types.
+ * This can be solved by using index properties for types and interfaces which are extended by that type.
+ */
+
+namespace IndexedPropertiesWithFixInterfaceProps {
+  /**
+   * First define that indexed type
+   */
+  type Colors = { [name: string]: string };
+
+  /**
+   * Second create an interface that extends the indexed type.
+   * Now all properties of that interface must be of type of the indexed type.
+   */
+  interface IColors extends Colors {
+    backgroundColor: string;
+    buttonColor: string;
+    color: string;
+    inputColor: string;
+  }
+}
