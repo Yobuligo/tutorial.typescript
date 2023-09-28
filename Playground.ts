2f5616ab295c28ba4ac6a75eeab8c3071a9123b2
Peter <hoffmannpeter82@gmail.com>
2023-09-28 08:49:22 +0200

    Add example how to structure repository

--- Playground.ts
+++ Playground.ts
@@ -1,17 +1,45 @@
-namespace Playground {
-  class ValueContainer<T> {
-    constructor(readonly value: T) {}
-  }
-
-  interface IPerson {
-    firstname: string;
-    age: number;
-  }
-
-  const valueContainer = new ValueContainer<IPerson>({
-    age: 28,
-    firstname: "Stacey",
-  });
-
-  debugger;
+interface IEntity {
+  id: number;
 }
+
+type IEntityDetails<T extends IEntity> = Omit<T, "id">;
+
+type 
+
+interface IBaseRepository<
+  TEntity extends IEntity,
+> {
+  deleteById(id: number): boolean;
+  findById(id: number): TEntity | undefined;
+}
+
+interface IRepository<TEntity extends IEntity> extends IBaseRepository<TEntity>{
+  add(entity: IEntityDetails<TEntity>): TEntity;
+  findAll(): TEntity[];  
+}
+
+interface ISubRepository<TEntity extends IEntity, TParent extends IEntity> extends IBaseRepository<TEntity>{
+  add(entity: IEntityDetails<TEntity>, parentId: number): TEntity;
+  findAll(parentId: number): TEntity[];
+}
+
+interface IBoard extends IEntity {
+  title: string;
+}
+
+interface IBoardRepository extends IRepository<IBoard> {}
+
+interface INote extends IEntity {
+  text: string;
+}
+
+interface INoteRepository extends ISubRepository<INote, IBoard> {}
+
+interface IVote extends IEntity {
+  type: number;
+}
+
+interface IVoteRepository extends ISubRepository<IVote, INote> {}
+
+const repo: IVoteRepository = {}
+repo.
