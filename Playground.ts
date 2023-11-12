dc17b05f0e538a5e99e308f6624e10b45f65963f
Yobuligo <hoffmannpeter82@gmail.com>
2023-11-12 22:29:42 +0100

    Add draft of infering type of function parameters

--- Playground.ts
+++ Playground.ts
@@ -1,17 +1,63 @@
 namespace Playground {
-  class ValueContainer<T> {
-    constructor(readonly value: T) {}
+  export type Decorator = Function;
+
+  class DecoratorInfoDefault {
+    private decorations: Map<Constructor<any>, Map<Decorator, any>> = new Map();
+
+    delete(type: Constructor<any>, decorator: Decorator) {
+      this.decorations.get(type)?.delete(decorator);
+    }
+
+    find<TDecorator extends Decorator, T extends Destructuring<TDecorator>>(
+      type: Constructor<any>,
+      decorator: TDecorator
+    ): T | undefined {
+      const decoration = this.decorations.get(type);
+
+      return decoration?.get(decorator);
+    }
+
+    introduce<T>(type: Constructor<any>, decorator: Decorator, value: T) {
+      const decoration =
+        this.decorations.get(type) ?? new Map<Decorator, any>();
+      decoration.set(decorator, value);
+      this.decorations.set(type, decoration);
+    }
+
+    has(type: Constructor<any>, decorator: Decorator): boolean {
+      return this.find(type, decorator) !== undefined;
+    }
   }
 
-  interface IPerson {
-    firstname: string;
-    age: number;
+  export const DecoratorInfo = new DecoratorInfoDefault();
+
+  type Destructuring<T> = T extends (...args: infer P) => any ? P : never;  
+
+  function Loader(path: string, second: number) {}
+
+  type Parameters<T> = T extends (...args: infer P) => any ? P : never;
+
+  function caller<
+    TDecorator extends (...args: any[]) => any,
+    T extends Parameters<TDecorator>
+  >(func: TDecorator): T {
+    return {} as T;
   }
 
-  const valueContainer = new ValueContainer<IPerson>({
-    age: 28,
-    firstname: "Stacey",
-  });
+  const myTuple: [firstname: string, age: number] = ["Stacey", 28]
 
-  debugger;
+  class Test{}
+
+
+  type T0 = string[];
+
+  type UnpackedArray<T> = T extends (infer U)[] ? U : T
+  type U0 = UnpackedArray<T0> // string
+
+  const test: UnpackedArray<["12", 12, 13]> = {} as UnpackedArray<["12", 12, 13]> 
+
+
+
+
+
 }
