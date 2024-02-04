bf284cf659ca5c77291a8cca9534fc9920402bf8
Peter <hoffmannpeter82@gmail.com>
2024-02-04 16:24:05 +0100

    Add lesson for documentation with markdown

---
+++ 03 documentation/03 markdown/app.ts
@@ -0,0 +1,20 @@
+/**
+ * documentation can contain markdown elements
+ * like Having a headline
+ * # My first headline
+ * ## My second headline
+ * 
+ * or using italic or bold
+ * **this should be bold**
+ * 
+ * *this should be italic*
+ * 
+ * And here comes some code 
+ * ```
+ * This is some test code
+ * function test(){
+ *      return "value"
+ * }
+ * ```
+ */
+class Test{}
