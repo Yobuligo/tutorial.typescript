946769579d815b404caa5ddf5bea820020327f40
Yobuligo <hoffmannpeter82@gmail.com>
2024-02-21 23:02:25 +0100

    Refactor lesson for finding placeholder params with colon from string

--- Playground.ts
+++ Playground.ts
@@ -1,29 +1,59 @@
 namespace Playground {
-  interface IValidator {
-    validate(): void;
+  /**
+   * What we need is a complex typing, which get the placeholders from a string
+   */
+  type IsParameter<Part> = Part extends `:${infer ParamName}`
+    ? ParamName
+    : never;
+  type FilteredParts<Path> = Path extends `${infer PartA}/${infer PartB}`
+    ? IsParameter<PartA> | FilteredParts<PartB>
+    : IsParameter<Path>;
+  type Params<Path> = {[Key in FilteredParts<Path>]: string};
+
+  /**
+   * Now we define a class, that represents a route
+   */
+  class Route<TParams extends string> {
+    constructor(readonly origin: TParams) {}
+
+    /**
+     * This methods converts the origin path by filling placeholders
+     */
+    toPath(params: Params<TParams>): string {
+      let path = this.origin;
+      for (const propName in params) {
+        path.replaceAll(`:${propName}`, (params as any)[propName]);
+      }
+      return path;
+    }
   }
 
-  const validatorProvider = (): IValidator => {
-    throw new Error();
+  /**
+   * Now we can define several routes. Therefore we provide a configure method to get the Route type safe.
+   */
+  type RoutesConfig = { [key: string]: Route<any> };
+  export const configure = <TRouteConfig extends RoutesConfig>(
+    config: TRouteConfig
+  ): TRouteConfig => {
+    return config;
   };
 
-  const validatorConsumer = ()=>{
-    
-  }
+  /**
+   * Next we can provide our Routes by our configure function.
+   * It would even be nicer if we only had to provide the path and the configure function returns an object with converts the path to objects of type Route.
+   * I tested it out, but here we lose the string itself, which means we are not able to get the parameter back
+   */
+  const Routes = configure({
+    home: new Route("/"),
+    persons: new Route("/persons/:id"),
+    personImage: new Route("/persons/:id/image"),
+  });
 
-
-  type ValidatorRequest = (validator: IValidator) => void;
-  const requestValidator = (validator: ValidatorRequest) => {};
-
-
-  requestValidator()
-
-
-
-
-
-
-
+  /**
+   * And finally we can either access the origin path or we can convert it to a new path which contains the placeholder values
+   */
+  Routes.persons.toPath({id:"Peter" });
+  Routes.persons.origin;
 
 
 
