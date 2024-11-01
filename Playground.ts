ece7ff8d0d54c45e4bb05ea133bc1293325870ea
Yobuligo <hoffmannpeter82@gmail.com>
2024-11-01 09:53:59 +0100

    Add documentation

--- Playground.ts
+++ Playground.ts
@@ -125,7 +125,7 @@ namespace Playground {
   type RoutesConfig = { [key: string]: IRoute<any> };
 
   /**
-   * This function is required to create an object that contains routes of type {@link IRoute} by the given {@link config}.
+   * This function is required to create an object using the given {@link config} which contains routes of type {@link IRoute}.
    * This config can be used to access the routes in a typesafe way. 
    * The goal is to have the route paths defined as literals only at one central point, this config.
    */
